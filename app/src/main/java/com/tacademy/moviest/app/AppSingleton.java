47e71020f34affd7eddce7a34090d0c91a5d66f2
ysera34 <ycyoon34@gmail.com>
2016-09-09 17:16:35 +0900

    add facebook social login

--- app/src/main/java/com/tacademy/moviest/app/AppSingleton.java
+++ app/src/main/java/com/tacademy/moviest/app/AppSingleton.java
@@ -8,7 +8,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
-
 /**
  * Created by yoon on 2016. 9. 7..
  */
---
+++ app/src/main/java/com/tacademy/moviest/view/activity/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.tacademy.moviest.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.tacademy.moviest.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        AccessToken token = loginResult.getAccessToken();
+                        String userId = token.getUserId();
+                        String tokenStr = token.getToken();
+
+                        Log.d("Token", "ID : " + userId + ", Token : " + tokenStr);
+                        startActivity(new Intent(LoginActivity.this, MoviesActivity.class));
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
